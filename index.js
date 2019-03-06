1fe4feb8e18b7ad4680ceab43b4ae9e8f4dae27a
Olusegun M. Isaac <oluisaac7@gmail.com>
2019-03-06 10:02:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-function getFirstSelector(selector) {
-  return document.querySelector(selector);
-}
+function getFirstSelector(selector){
+  return document.querySelector(selector)
 
-function nestedTarget() {
-  return document.getElementById("nested").querySelector(".target");
-}
+ }
 
-function increaseRankBy(n) {
-  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
-  for(let i=0;i<lis.length;i++) {
-    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
-  }
-}
+ function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
 
-function deepestChild() {
-  var lis=document.getElementById("grand-node").querySelectorAll("div");
-  var test;
-  for(let i=0;i<lis.length-1;i++) {
-    test=lis[i].querySelector("div");
-  }
-return test;
-}
+ }
+function increaseRankBy(n){
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+
+   for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+    }
+
+ }
+
+ function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
