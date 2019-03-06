fbce7faec16e150f4cf48df13bdb2577d774bddd
Olusegun M. Isaac <oluisaac7@gmail.com>
2019-03-06 10:23:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function  nestedTarget(){
   return document.target('nested').querySelector('div.target')
 }
 function deepestChild(){
-  
+
 }
 function increaseRankBy(n){
   const upRank==
   document.getElementById('app') .querySelectorAll ('ul.ranked-list')
-  
+
   for (let  // IDEA:  (true) {
-    
+
   } = 0; // IDEA:  <upRank.length;  i++){
     upRank [// IDEA:].innerHTML =((parseInt(upRank[i].innerHTML))  + n)
   }
