b7a0e3a105711b137c12b94fc65b27d5a0b75dc2
Olusegun M. Isaac <oluisaac7@gmail.com>
2019-03-06 10:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById("nested").querySelector(".target");
+  return document.getElementById("#nested").querySelector(".target");
 }
 
 function increaseRankBy(n) {
-  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  var lis=document.getElementById("#app").querySelectorAll("ul.ranked-list li");
   for(let i=0;i<lis.length;i++) {
     lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
   }
 }
 
 function deepestChild() {
-  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var lis=document.getElementById("#grand-node").querySelectorAll("div");
   var test;
   for(let i=0;i<lis.length-1;i++) {
-    test=lis[i].querySelector("div");
+    test=lis[i].querySelector("#div");
   }
 return test;
 }
