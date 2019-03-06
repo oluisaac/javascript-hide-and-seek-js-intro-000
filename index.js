302421a7ffd2d699f6d58e126bcd9ee954d97b18
Olusegun M. Isaac <oluisaac7@gmail.com>
2019-03-06 10:21:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function getFirstSelector(selector){
+return document.querySelector(selector)
+  }
+function  nestedTarget(){
+  return document.target('nested').querySelector('div.target')
+}
+function deepestChild(){
+  
+}
+function increaseRankBy(n){
+  const upRank==
+  document.getElementById('app') .querySelectorAll ('ul.ranked-list')
+  
+  for (let  // IDEA:  (true) {
+    
+  } = 0; // IDEA:  <upRank.length;  i++){
+    upRank [// IDEA:].innerHTML =((parseInt(upRank[i].innerHTML))  + n)
+  }
+}
