cee476d925d961a70c5cf8d420fe7c51dda12181
Olusegun M. Isaac <oluisaac7@gmail.com>
2019-03-06 10:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 function getFirstSelector(selector){
-return document.querySelector(selector)
-  }
-function  nestedTarget(){
-  return document.target('nested').querySelector('div.target')
-}
-function deepestChild(){
+  return document.querySelector(selector)
 
-}
+ }
+
+ function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+
+ }
 function increaseRankBy(n){
-  const upRank==
-  document.getElementById('app') .querySelectorAll ('ul.ranked-list')
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
 
-  for (let  // IDEA:  (true) {
+   for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+    }
 
-  } = 0; // IDEA:  <upRank.length;  i++){
-    upRank [// IDEA:].innerHTML =((parseInt(upRank[i].innerHTML))  + n)
-  }
-  function deepestChild(){
+ }
+
+ function deepestChild(){
   var deep = document.getElementById('grand-node').querySelectorAll('div')
   return deep[deep.length-1]
-}
